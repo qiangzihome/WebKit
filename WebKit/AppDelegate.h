5bf89f81848a9399ed6fdb117f092d826ab42e29
liqiang <qiangzihome@hotmail.com>
2015-05-19 09:46:34 +0800

    框架

---
+++ WebKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WebKit
+//
+//  Created by liqiang on 5/19/15.
+//  Copyright (c) 2015 liqiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WebKit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WebKit
+//
+//  Created by liqiang on 5/19/15.
+//  Copyright (c) 2015 liqiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
